af4ff1d106fdbbdcbeaab0cb4017ed326de4a3b4
Abdirahman Ali <abdirahman.ali@abdirah-ltm4tx2.internal.salesforce.com>
2022-06-27 08:08:38 -0700

    changed some components

--- student-store-ui/src/components/ProductCard/ProductCard.jsx
+++ student-store-ui/src/components/ProductCard/ProductCard.jsx
@@ -1,4 +1,47 @@
 import * as React from "react";
+import "./ProductCard.css";
 import { Link } from "react-router-dom";
+import ShoppingCart from "../ShoppingCart/ShoppingCart";
 
-export default function ProductCard(props) {}
+export default function ProductCard({
+  product,
+  productId,
+  quantity,
+  handleAddItemToCart,
+  handleRemoveItemToCart,
+  showDescription,
+}) {
+  const description = showDescription ? (
+    <div className="product-description">{product.description}</div>
+  ) : (
+    ""
+  );
+  return (
+    <div className="product-card">
+      {description}
+
+      <Link to={"/products/" + productId}>
+        <img
+          src={product.image}
+          height="200px"
+          width="200px"
+          className="image"
+        />
+      </Link>
+      <p className="product-name">{product.name}</p>
+      <p className="product-price">${product.price}</p>
+      <p className="product-description">
+        {showDescription ? description : ""}
+      </p>
+
+      <button className="add" onClick={() => handleAddItemToCart}>
+        {"+"}
+      </button>
+      <button className="remove" onClick={() => handleRemoveItemToCart}>
+        {"-"}
+      </button>
+
+      <div className="product-quantity">{quantity}</div>
+    </div>
+  );
+}
